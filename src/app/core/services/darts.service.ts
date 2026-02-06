915532edc984d8b39ff50b86a7bb779bfbc0712b
Janus Maritz <janus.maritz7@gmail.com>
2026-02-06 12:09:37 +0200

    add service

---
+++ src/app/core/services/darts.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { PlayerProfile } from '../models/player-profile.model';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({ providedIn: 'root' })
+export class DartsService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/darts`;
+
+  getPlayers() {
+    return this.http.get<PlayerProfile[]>(`${this.apiUrl}/players`);
+  }
+}
