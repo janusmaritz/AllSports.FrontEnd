59fdc791f1ad7ea8c3a8e3520b30f399ca18d3cc
Janus Maritz <janus.maritz7@gmail.com>
2026-02-06 14:04:12 +0200

    Add sidebar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayerList } from "./core/features/player-list/player-list";
+import { SidebarComponent } from './core/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PlayerList],
+  imports: [RouterOutlet, PlayerList, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.allsports.com/api'
+  apiUrl: 'allsportsapi-as-hqhaexe6h5h9f7a5.southafricanorth-01.azurewebsites.net'
 };
