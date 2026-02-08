dea31cc00c20a320fb148c5b42f16d55cb120bcb
Janus Maritz <janus.maritz7@gmail.com>
2026-02-08 19:59:38 +0200

    Change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'allsportsapi-as-hqhaexe6h5h9f7a5.southafricanorth-01.azurewebsites.net'
+  apiUrl: 'https://allsportsapi-as-hqhaexe6h5h9f7a5.southafricanorth-01.azurewebsites.net/api'
 };
